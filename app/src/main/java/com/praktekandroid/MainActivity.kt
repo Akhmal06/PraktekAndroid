92f8b0907bbb0be2a6dc0857a5389123dfbf97a2
akhmal12 <akhmdbhtmi@gmail.com>
2020-05-14 14:23:52 -0700

    Game ON

--- app/src/main/java/com/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/praktekandroid/MainActivity.kt
@@ -25,5 +25,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        button2.setOnClickListener{
+            val intent  = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
